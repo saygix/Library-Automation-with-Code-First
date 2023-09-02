e679dc215f9b5224e48814701bdce5913217f771
saygix <139467552+saygix@users.noreply.github.com>
2023-09-02 23:27:50 +0300

    Create İade.cs

---
+++ Entity/İade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDP.Entity
+{
+    class İade
+    {
+        [Key]
+        public int Id { get; set; }
+        public string KullaniciAdi { get; set; }
+
+        public string İadeTarihi { get; set; }
+      
+        public string KitapAdi { get; set; }
+    }
+}
