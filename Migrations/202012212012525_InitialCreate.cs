d0dd8b06a5e1b3f631c28844fdda797cfd7585ce
saygix <139467552+saygix@users.noreply.github.com>
2023-09-02 23:27:30 +0300

    Create 202012212012525_InitialCreate.cs

---
+++ Migrations/202012212012525_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿namespace NDP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Kullanicis",
+                c => new
+                    {
+                        tc = c.String(nullable: false, maxLength: 128),
+                        ad = c.String(),
+                        soyad = c.String(),
+                        dtarihi = c.String(),
+                        telno = c.String(),
+                        sifre = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.tc);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Kullanicis");
+        }
+    }
+}
