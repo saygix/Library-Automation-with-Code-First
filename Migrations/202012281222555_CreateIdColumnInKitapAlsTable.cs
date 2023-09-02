2f71db2fac0ba1ec4690867fadd48d96c82100d3
saygix <139467552+saygix@users.noreply.github.com>
2023-09-02 23:26:58 +0300

    Create 202012281222555_CreateIdColumnInKitapAlsTable.cs

---
+++ Migrations/202012281222555_CreateIdColumnInKitapAlsTable.cs
@@ -0,0 +1,24 @@
+﻿namespace NDP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateIdColumnInKitapAlsTable : DbMigration
+    {
+        public override void Up()
+        {
+            DropPrimaryKey("dbo.KitapAls");
+            AddColumn("dbo.KitapAls", "Id", c => c.Int(nullable: false, identity: true));
+            AlterColumn("dbo.KitapAls", "KitapAdi", c => c.String());
+            AddPrimaryKey("dbo.KitapAls", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropPrimaryKey("dbo.KitapAls");
+            AlterColumn("dbo.KitapAls", "KitapAdi", c => c.String(nullable: false, maxLength: 128));
+            DropColumn("dbo.KitapAls", "Id");
+            AddPrimaryKey("dbo.KitapAls", "KitapAdi");
+        }
+    }
+}
