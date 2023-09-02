57294bbbb88975d4b36a6492decc447dafa83bb8
saygix <139467552+saygix@users.noreply.github.com>
2023-09-02 23:27:54 +0300

    Create Context.cs

---
+++ Entity/Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDP.Entity
+{
+    class Context:DbContext
+    {
+        public DbSet<Kullanici> Kullanicis { get; set; }
+        public DbSet<Yonetici> Yoneticis { get; set; }
+        public DbSet<Kitap> Kitaps { get; set; }
+        public DbSet<KitapAl> KitapAls { get; set; }
+        public DbSet<İade> İades { get; set; }
+
+
+
+
+    }
+}
