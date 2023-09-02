b3738c9243fd976f51670008ef586665de9670f9
saygix <139467552+saygix@users.noreply.github.com>
2023-09-02 23:27:41 +0300

    Create KitapAl.cs

---
+++ Entity/KitapAl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDP.Entity
+{
+    class KitapAl
+    {
+        [Key]
+        public int Id { get; set; }
+        public string KullaniciAdi { get; set; }
+        public string KitapAdi { get; set; }
+        public string AldıgıTarih { get; set; }
+    }
+}
