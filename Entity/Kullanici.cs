c29eba70cb0c848b43d5afab99dd2358d1bd4798
saygix <139467552+saygix@users.noreply.github.com>
2023-09-02 23:27:38 +0300

    Create Kullanici.cs

---
+++ Entity/Kullanici.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDP.Entity
+{
+    class Kullanici
+    {
+        [Key]
+        public string tc { get; set; }
+        public string ad { get; set; }
+        public string soyad { get; set; }
+        public string dtarihi { get; set; }
+        public string telno { get; set; }
+        public int sifre { get; set; }
+    }
+}
