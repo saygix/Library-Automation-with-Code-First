e90bea344cacee944308c25b2503d39d031127f4
saygix <139467552+saygix@users.noreply.github.com>
2023-09-02 23:27:07 +0300

    Create 202012281219090_CreateIdColumnInİadeTable.cs

---
+++ Migrations/202012281219090_CreateIdColumnInİadeTable.cs
@@ -0,0 +1,32 @@
+﻿namespace NDP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateIdColumnInİadeTable : DbMigration
+    {
+        public override void Up()
+        {
+            DropPrimaryKey("dbo.İade");
+            DropPrimaryKey("dbo.KitapAls");
+            AddColumn("dbo.İade", "Id", c => c.Int(nullable: false, identity: true));
+            AlterColumn("dbo.İade", "KullaniciAdi", c => c.String());
+            AlterColumn("dbo.KitapAls", "KullaniciAdi", c => c.String());
+            AlterColumn("dbo.KitapAls", "KitapAdi", c => c.String(nullable: false, maxLength: 128));
+            AddPrimaryKey("dbo.İade", "Id");
+            AddPrimaryKey("dbo.KitapAls", "KitapAdi");
+        }
+        
+        public override void Down()
+        {
+            DropPrimaryKey("dbo.KitapAls");
+            DropPrimaryKey("dbo.İade");
+            AlterColumn("dbo.KitapAls", "KitapAdi", c => c.String());
+            AlterColumn("dbo.KitapAls", "KullaniciAdi", c => c.String(nullable: false, maxLength: 128));
+            AlterColumn("dbo.İade", "KullaniciAdi", c => c.String(nullable: false, maxLength: 128));
+            DropColumn("dbo.İade", "Id");
+            AddPrimaryKey("dbo.KitapAls", "KullaniciAdi");
+            AddPrimaryKey("dbo.İade", "KullaniciAdi");
+        }
+    }
+}
