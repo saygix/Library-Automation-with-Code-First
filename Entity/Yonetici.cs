926121add63c1d39443cc8c7f14e85b54b031894
saygix <139467552+saygix@users.noreply.github.com>
2023-09-02 23:27:34 +0300

    Create Yonetici.cs

---
+++ Entity/Yonetici.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDP.Entity
+{
+    class Yonetici
+    {
+        [Key]
+        public string ad { get; set; }
+        public int sifre { get; set; }
+
+    }
+}
