3de9294d597b0fee111402819216d7c4902f7f58
saygix <139467552+saygix@users.noreply.github.com>
2023-09-02 23:27:17 +0300

    Create 202012261838074_CreateKullaniciAdiColumnInKitapAlsTable.cs

---
+++ Migrations/202012261838074_CreateKullaniciAdiColumnInKitapAlsTable.cs
@@ -0,0 +1,24 @@
+﻿namespace NDP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateKullaniciAdiColumnInKitapAlsTable : DbMigration
+    {
+        public override void Up()
+        {
+            DropPrimaryKey("dbo.KitapAls");
+            AddColumn("dbo.KitapAls", "KullaniciAdi", c => c.String(nullable: false, maxLength: 128));
+            AlterColumn("dbo.KitapAls", "KitapAdi", c => c.String());
+            AddPrimaryKey("dbo.KitapAls", "KullaniciAdi");
+        }
+        
+        public override void Down()
+        {
+            DropPrimaryKey("dbo.KitapAls");
+            AlterColumn("dbo.KitapAls", "KitapAdi", c => c.String(nullable: false, maxLength: 128));
+            DropColumn("dbo.KitapAls", "KullaniciAdi");
+            AddPrimaryKey("dbo.KitapAls", "KitapAdi");
+        }
+    }
+}
