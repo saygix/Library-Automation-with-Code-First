babb981c8538ee86943016b8007e3ffbb4dd8c12
saygix <139467552+saygix@users.noreply.github.com>
2023-09-02 23:26:44 +0300

    Create Configuration.cs

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,25 @@
+﻿namespace NDP.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<NDP.Entity.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "NDP.Entity.Context";
+        }
+
+        protected override void Seed(NDP.Entity.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
