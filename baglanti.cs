b4731b7a9e21be1d94e3579434a92dfe5248f48e
Ayşegül Saygı <139467552+saygix@users.noreply.github.com>
2023-09-12 23:56:22 +0300

    Create baglanti.cs

---
+++ baglanti.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDP
+{
+  public static class baglanti
+    {
+        public static string baglnti = "Server=localhost\\SQLEXPRESS03;Database=NDP;Trusted_Connection=True";
+    }
+}
